14af90376ac70b3a723d4e3c9de602eced93d335
gjacqual <gjacqual@student.21-school.ru>
2022-06-21 14:00:54 +0300

    add exercises 1 and 2

---
+++ ex01-2/test.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int	main(void)
+{
+	printf("\\t: <\t>\n"); /* tab */
+	printf("\\b: <\b>\n"); /* backspace */
+	printf("\\\\: <\\>\n"); /* косая черта */
+	printf("\\v: <\v>\n");	/* vertical tab */
+	printf("\\a: <\a>\n"); /* подчача звукового сигнала */
+	printf("\\f: <\f>\n"); /*прогон страницы */
+	printf("\\r: <\r>\n"); /* возврат каретки */
+	printf("\\?: <\?>\n"); /* вопросительный знак */
+	printf("\\': <\'>\n"); /* одинарная кавычка */
+	printf("\\\": <\">\n"); /* двойная кавычка */
+	printf("\\xhh: <\xFF>\n"); /* число */
+	printf("\\e: <\e>\n"); /* Escape*/
+	printf("\\u: <\uFFFF>\n"); /* число */
+	printf("\\x: <\xFF>\n"); /* число */
+
+	// printf("\\0: <\0>\n"); /* Нулевой байт */
+	// printf("\\y: <\y>\n");
+	// printf("\\q: <\q>\n"); 
+	// printf("\\w: <\w>\n");
+	// printf("\\c: <\c>\n");
+	// printf("\\ooo: <\o>\n");
+	// printf("\\i: <\i>\n");
+	// printf("\\p: <\pFFF>\n");
+	// printf("\\{: <\{}>\n");
+	// printf("\\s: <\s>\n");
+	// printf("\\d: <\d>\n");
+	// printf("\\g: <\g>\n");
+	// printf("\\h: <\h>\n");
+	// printf("\\j: <\j>\n");
+	// printf("\\k: <\k>\n");
+	// printf("\\l: <\l>\n");
+	// printf("\\z: <\z>\n");
+	// printf("\\m: <\m>\n");
+	// printf("\\,: <\,>\n");
+	// printf("\\.: <\.>\n");
+	// printf("\\/: <\/>\n");
+ 
+
+
+	return (0);
+}

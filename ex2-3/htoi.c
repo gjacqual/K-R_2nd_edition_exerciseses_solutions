ef1f91af373d9cc36c75dd836a6f916e94a6c74c
gjacqual <gjacqual@student.21-school.ru>
2022-07-10 16:04:24 +0300

    start do ex 2-3

---
+++ ex2-3/htoi.c
@@ -0,0 +1,11 @@
+/*
+ * K&R ex2-2
+ * 
+ * clang -Wall -Wextra -Werror -o ex2-2 ex2-2.c
+ * 
+ * Exercise 2-2 Write a function htoi(), which converts a string of hexadecimal digits 
+ * (including an optional 0x or 0X)into its equivalent integer value. 
+ * The allowable digits are 0 through 9, a through f, and A through F.
+ *
+ * Max Filin 2022
+ */

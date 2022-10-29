ce9579e70cee00faf2e3b9a4b69ab1076e511b47
gjacqual <gjacqual@student.21-school.ru>
2022-10-29 12:54:52 +0300

    ex04-04 ready

--- ex04-04/my_calculator.c
+++ ex04-04/my_calculator.c
@@ -1,13 +1,13 @@
 /*
- * K&R ex4-3
+ * K&R ex4-4
  *
  * clang -Wall -Wextra -Werror -o my_calculator my_calculator.c -lm
  * 
  * run: ./my_calculator
  *
- * Exercise 4-3 Given the basic framework, it's straightforward to extend the 
- * calculator. Add the modulus ( % ) operator and provisions for negative 
- * numbers.
+ * Exercise 4-4 Add commands to print the top element of the stack without 
+ * popping, to duplicate it, and to swap the top two elements. Add a command to 
+ * clear the stack.
  *
  * Max Filin 2022
  */
@@ -25,25 +25,31 @@
 
 #define MAXVAL	100 /* максимальная глубина стека val */
 
-int sp = 0;			/* следующая свободная позиция в стеке */
-double val[MAXVAL];	/* стек операндов */
-char buf[BUFSIZE]; /* буфера для ungetch */
-int bufp = 0;
+int		sp = 0;			/* следующая свободная позиция в стеке */
+double	val[MAXVAL];	/* стек операндов */
+char	buf[BUFSIZE]; /* буфера для ungetch */
+int		bufp = 0;
 
-int getop(char s[]);
-void push(double);
-double pop(void);
-int 	getch(void);
+int		getop(char s[]);
+void	push(double);
+double	pop(void);
+int		getch(void);
 void	ungetch(int);
-bool isint(double d);
+bool	isint(double d);
+void	print_top(void);
+void	print_stack(void);
+void	clear_stack(void);
+void	doublicate_element(double f);
+void	swap_elements(void);
 
 /* Калькулятор с обратной польской записью */
-int main(void)
+int	main(void)
 {
-	int type;
-	double op2, op;
-	char s[MAXOP];
+	int		type, command_flag;
+	double	op2, op;
+	char	s[MAXOP];
 
+	command_flag = 0;
 	while ((type = getop(s)) != EOF)
 	{
 		switch(type)
@@ -77,17 +83,81 @@ int main(void)
 				else
 					printf("error: zero devisor\n");
 				break;
+			case 'T':
+				command_flag = 1;
+				print_top();
+				break;
+			case 'C':
+				command_flag = 1;
+				clear_stack();
+				break;
+			case 'D':
+				command_flag = 1;
+				doublicate_element(val[0]);
+				break;
+			case 'S':
+				command_flag = 1;
+				swap_elements();
+				break;
+			case 'P':
+				command_flag = 1;
+				print_stack();
+				break;
 			case '\n':
-				printf("\t%.8g\n", pop());
+				if (!command_flag)
+					printf("\t%.8g\n", pop());
+				else 
+					command_flag = 0;
 				break;
 			default:
 				printf("error: unknown command %s\n", s);
 				break;
 		} 
 	}
+	
 	return (0);
 }
 
+void swap_elements(void)
+{
+	double temp;
+
+	temp = val[0];
+	val[0] = val[1];
+	val[1] = temp;
+}
+
+void doublicate_element(double f)
+{
+	int temp_sp = sp;
+	if (temp_sp < MAXVAL)
+		val[++sp] = f;
+	else
+		printf("error: stack full, can't push %g\n", f);
+}
+
+void print_stack(void)
+{
+	for (int i = 0; i < 10; i++)
+		printf("%g\t", val[i]);
+	printf("\n");
+	// printf("sp: %d\n", sp);
+}
+
+void clear_stack(void)
+{
+	int temp_sp;
+
+	temp_sp = sp + 1;
+	while(temp_sp >= 0)
+		val[temp_sp--] = 0.0;
+	sp = 0;
+}
+
+void print_top(void)
+{
+	printf("\t%.8g\n", val[0]);
+}
 
 bool isint(double d) 
 {
